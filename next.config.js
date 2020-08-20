bf2ec645b0bfda76ac3d0f6a73c0cf6e90d36c31
revengemiroz <mirozxy@gmail.com>
2020-08-20 23:52:25 +0545

    some fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // next.config.js
 const withImages = require('next-images')
 module.exports = withImages({
+    inlineImageLimit: 16384,
     webpack(config, options) {
         return config
     }
