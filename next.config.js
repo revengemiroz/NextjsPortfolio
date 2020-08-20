43b3ab4f07b07ad195387b02911cd3a289fd4a93
revengemiroz <mirozxy@gmail.com>
2020-08-20 23:48:13 +0545

    some fixes

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
+// next.config.js
 const withImages = require('next-images')
-module.exports = withImages()
+module.exports = withImages({
+    webpack(config, options) {
+        return config
+    }
+})
