6b48cbe8e64c74b5e54cdf8d988be944680feb41
revengemiroz <mirozxy@gmail.com>
2020-08-23 15:05:36 +0545

    footer upgrades

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -13,7 +13,22 @@ function Layout({ children, page, title, image }) {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <meta property='og:title' content={title} key={title} />
                 <meta key='og:image' name='og:image' content={image} />
+
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176053043-1"></script>
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                        
+                        
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'UA-176053043-1');
+                        
+                `
+                }} />
             </Head>}
+
 
             <div>
                 <main className={styles.container}>
